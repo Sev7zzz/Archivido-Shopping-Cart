e754c494cfdde4810a11b6d696a7dea1314792b6
Giuseppe <giuseppearchivido@gmail.com>
2025-02-03 11:47:59 +0800

    final change

--- main.py
+++ main.py
@@ -5,20 +5,35 @@ app = FastAPI()
 
 # Define a Pydantic model
 class Item(BaseModel):
+    id: int
     name: str
-
+    price: float
+    quantity: int
 
 # Create a list to hold items
+itemsList = [
+    {"id": 1, "name": "Orange", "price": 25.0, "quantity": 5},
+    {"id": 2, "name": "Apple", "price": 30.0, "quantity": 10},
+    {"id": 3, "name": "Banana", "price": 15.0, "quantity": 20},
+    {"id": 4, "name": "Mango", "price": 50.0, "quantity": 8},
+    {"id": 5, "name": "Grapes", "price": 40.0, "quantity": 12},
+]
 
-itemsList = {"orange", "banana", "watermelon", "strawberry"}
-items = []
+#Holds the items
+ShoppingCartItems = []
 
+# Endpoint to get the items from the cart
+@app.get("/cart/")
+def read_items():
+ return {"Shopping Cart": ShoppingCartItems}
+
+# Endpoint to the get items
 @app.get("/fruits/")
 def read_items():
     return {"Fruits": itemsList}
 
-# If you want to add an endpoint to add items
+# Endpoint to add items
 @app.post("/items/")
 def add_item(item: Item):
-    items.append(item)
+    ShoppingCartItems.append(item)  # Convert Pydantic model to dict
     return {"message": "Item added successfully", "item": item}
